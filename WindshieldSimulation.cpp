8f3e6a737f74f98a2d829c89615a54e65dda0363
Hannah <hbsarver@gmail.com>
2016-04-23 12:08:15 -0700

    Add scheduler skeleton to windshield sim

--- WindshieldSimulation.cpp
+++ WindshieldSimulation.cpp
@@ -2,18 +2,22 @@
 #include "giotto.h"
 #include "ptides.h"
 #include "actors.h"
+#include "scheduler.h"
 
 int main() {
 	cout << "Windshield Simulation Program for EE249B Project" << endl;
 
+	// Start a scheduler
+	Scheduler scheduler = new Scheduler();
+
 	// Giotto simulation
-	GiottoDirector giottoD = new GiottoDirector();
+	GiottoDirector giottoD = new GiottoDirector(scheduler);
 	cout << "Starting Simulation Using Giotto" << endl;
 	// Fill in - initialize stuff and start the director running...
 
 	// Ptides simulation
-	PtidesDirector ptidesD = new PtidesDirector();
-	cout << "Starting Simulation Using Ptides" << endl;
+	// PtidesDirector ptidesD = new PtidesDirector();
+	// cout << "Starting Simulation Using Ptides" << endl;
 	// Fill in - initialize stuff and start the director running...
 
 	return 0;
