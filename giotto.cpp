de9ab825212fbc5cbd838e487a6c5c491bbda699
Hannah <hbsarver@gmail.com>
2016-04-23 18:18:06 -0700

    Modifications to giotto.h and skeleton giotto.cpp

--- giotto.cpp
+++ giotto.cpp
@@ -3,4 +3,90 @@
 #include <string>
 #include <giotto.h>
 
-//	Define things here
+using namespace std;
+
+//	Giotto components definitions
+
+Port::Port(string nameIn, string typeIn) {
+	name = nameIn;
+	SetType(typeIn);
+	initialValue = 0; // which type?
+}
+
+void Port::SetType(string typeIn) {
+	type = typeIn;
+}
+
+Task::Task(string nameIn) {
+	name = nameIn;
+}
+
+vector<Port> Task::TaskFunction(vector<Port> pVector) {
+	// ...
+}
+
+Driver::Driver(string nameIn) {
+	name = nameIn;
+}
+
+bool Driver::DriverFunction(vector<Port.value> pValVector) {
+	// ...
+}
+
+bool Driver::Guard(vector<Port.value> pValVector) {
+	// ...
+}
+
+Mode::Mode(string nameIn) {
+	name = nameIn;
+}
+
+void Mode::SetStartMode(Mode m) {
+	startMode = m;
+}
+
+TaskInvocation::TaskInvocation(Task t, Driver d, unsigned int f) {
+	myTask = t;
+	myDriver = d;
+	frequency = f;
+}
+
+void TaskInvocation::SetFrequency (unsigned int f) {
+	frequency = f;
+}
+
+ActuatorUpdate::ActuatorUpdate(Driver d, unsigned int f) {
+	myDriver = d;
+	frequency = f;
+}
+
+void ActuatorUpdate::SetFrequency (unsigned int f) {
+	frequency = f;
+}
+
+ModeSwitch::ModeSwitch(Driver d, Mode m, unsigned int f) {
+	myDriver = d;
+	targetMode = m;
+	frequency = f;
+}
+
+void ModeSwitch::SetFrequency (unsigned int f) {
+	frequency = f;
+}
+
+void ModeSwitch::SetTargetMode (Mode m) {
+	targetMode = m;
+}
+
+Config::Config () {
+	// ...
+}
+
+GiottoDirector::GiottoDirector(&Scheduler scheduler) { // check syntax
+	myScheduler = scheduler;
+	// ...
+}
+
+void GiottoDirector::Run(vector<Task> activeTasks) {
+	// ...
+}
