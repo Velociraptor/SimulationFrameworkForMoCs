c64a410abc1de4873247d5d97d934e10a98707e1
Hannah <hbsarver@gmail.com>
2016-04-27 19:40:41 -0700

    Updating drivers to Guards

--- WindshieldSimulation.cpp
+++ WindshieldSimulation.cpp
@@ -10,26 +10,31 @@ int main() {
 	// Giotto simulation
 	cout << "Starting Simulation Using Giotto" << endl;
 
+	// Define trivially true guard
+	bool trivialTrueGuard () {
+		return true;
+	}
+
 	// Weather Model simulates rainfall as a random int per [unit time]
 	RandomIntInRange weatherModel = new RandomIntInRange("WeatherModel",20);
 	Task generateRainfall = new Task("MakeItRain", weatherModel.Compute());
-	Driver rainDriver = new Driver("RainDriver"); // ??? what else to set?
+	Guard rainGuard = new Guard("RainGuard", trivialTrueGuard);
 	unsigned int rainGenFreq = 2000;
-	TaskInvocation genRainInvoke = new TaskInvocation(generateRainfall, rainDriver, rainGenFreq);
+	TaskInvocation genRainInvoke = new TaskInvocation(generateRainfall, rainGuard, rainGenFreq);
 
 	// Rainfall Sensor accumulates rain over time to dictate rate of fall for controls
 	AccumulatorWithReset rainfallSensor = new AccumulatorWithReset("RainfallSensor");
 	Task senseRainfall = new Task("HaveYouEverFeltTheRain", rainfallSensor.Compute());
-	Driver sensorDriver = new Driver("SensorDriver"); // ???
+	Guard sensorGuard = new Guard("SensorGuard", trivialTrueGuard);
 	unsigned int rainSenseFreq = 2000;
-	TaskInvocation senseRainInvoke = new TaskInvocation(senseRainfall, sensorDriver, rainSenseFreq);
+	TaskInvocation senseRainInvoke = new TaskInvocation(senseRainfall, sensorGuard, rainSenseFreq);
 	
 	// Rainfall Rate tracks rainfall over [unit time] to send to mode control
 	Difference rainfallRateCheck = new Difference("RainfallRateCheck");
 	Task checkRainfallRate = new Task("RainingCatsOrDogs", rainfallRateCheck.Compute());
-	Driver rainRateDriver = new Driver("rainRateDriver"); // ???
+	Guard rainRateGuard = new Guard("rainRateGuard", trivialTrueGuard);
 	unsigned int rainRateCheckFreq = 2000;
-	TaskInvocation rainRateInvoke = new TaskInvocation(checkRainfallRate, rainRateDriver, rainRateCheckFreq);
+	TaskInvocation rainRateInvoke = new TaskInvocation(checkRainfallRate, rainRateGuard, rainRateCheckFreq);
 
 	// Modes and switching logic for wiper speed based on tracked rate of rainfall
 	Mode lowRainMode = new Mode(); // ???
@@ -47,32 +52,61 @@ int main() {
 	highRainTaskList.push_back(genRainInvoke);
 	highRainTaskList.push_back(senseRainInvoke);
 	highRainTaskList.push_back(rainRateInvoke);
+
 	// Wiper actuation based on operating mode
 	// (wiper action is to reset sensor accumulator)
-	Task actuateWiper new Task("WipeItOff", rainfallSensor.Reset());
-	Driver wiperDriver = new Driver("WiperDriver"); // ???
+	Task actuateWiper new Task("WipeItOff", rainfallSensor.GetInputs()[1]->SetValueBool(true));
+	Guard wiperGuard = new Guard("WiperGuard", trivialTrueGuard); // ???
 	unsigned int lowWiperFreq = 250;
-	TaskInvocation lowWiperInvoke = new TaskInvocation(actuateWiper, wiperDriver, lowWiperFreq);
+	TaskInvocation lowWiperInvoke = new TaskInvocation(actuateWiper, wiperGuard, lowWiperFreq);
 	lowRainTaskList.push_back(lowWiperInvoke);
 	unsigned int medWiperFreq = 500;
-	TaskInvocation medWiperInvoke = new TaskInvocation(actuateWiper, wiperDriver, medWiperFreq);
+	TaskInvocation medWiperInvoke = new TaskInvocation(actuateWiper, wiperGuard, medWiperFreq);
 	medRainTaskList.push_back(medWiperInvoke);
 	unsigned int highWiperFreq = 1000;
-	TaskInvocation highWiperInvoke = new TaskInvocation(actuateWiper, wiperDriver, highWiperFreq);
+	TaskInvocation highWiperInvoke = new TaskInvocation(actuateWiper, wiperGuard, highWiperFreq);
 	highRainTaskList.push_back(highWiperInvoke);
 	// ...
 
-	// ????
+	// Define guard functions
+	bool rainRateLow () {
+		// rainfall under 30 from last check
+		int currentRainRate = rainfallRateCheck.GetOutputs()[0]->GetValueInt();
+		if (currentRainRate < 34) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	bool rainRateMed () {
+		// rainfall under 30 from last check
+		int currentRainRate = rainfallRateCheck.GetOutputs()[0]->GetValueInt();
+		if (currentRainRate > 33 && currentRainRate < 67) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	bool rainRateHigh () {
+		// rainfall under 30 from last check
+		int currentRainRate = rainfallRateCheck.GetOutputs()[0]->GetValueInt();
+		if (currentRainRate > 66) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	// Mode switches *to* each possible mode, 
-	// with Drivers that have the appropriate guards 
+	// with appropriate Guards 
 	// and a frequency as often as we want to be able to switch modes...
 	unsigned int modeSwitchCheckFreq = 100;
-	Driver lowRainDriver = new Driver("lowRainDriver"); // set guard: not already in low, and rainfall rate check in low range
-	ModeSwitch wiperSpeedLowSwitch = new ModeSwitch(lowRainDriver, lowRainMode, modeSwitchCheckFreq);
-	Driver medRainDriver = new Driver("medRainDriver"); // set guard: not already in med, and rainfall rate check in med range
-	ModeSwitch wiperSpeedMedSwitch = new ModeSwitch(medRainDriver, medRainMode, modeSwitchCheckFreq);
-	Driver highRainDriver = new Driver("highRainDriver"); // set guard: not already in high, and rainfall rate check in high range
-	ModeSwitch wiperSpeedHighSwitch = new ModeSwitch(highRainDriver, highRainMode, modeSwitchCheckFreq);
+	Guard lowRainGuard = new Guard("lowRainGuard", rainRateLow); // set guard: rainfall rate check in low range
+	ModeSwitch wiperSpeedLowSwitch = new ModeSwitch(lowRainGuard, lowRainMode, modeSwitchCheckFreq);
+	Guard medRainGuard = new Guard("medRainGuard", rainRateMed); // set guard: rainfall rate check in med range
+	ModeSwitch wiperSpeedMedSwitch = new ModeSwitch(medRainGuard, medRainMode, modeSwitchCheckFreq);
+	Guard highRainGuard = new Guard("highRainGuard", rainRateHigh); // set guard: rainfall rate check in high range
+	ModeSwitch wiperSpeedHighSwitch = new ModeSwitch(highRainGuard, highRainMode, modeSwitchCheckFreq);
 
 	// More mode setup
 	// Set invocations for each mode to xxxWiperInvoke
@@ -95,19 +129,9 @@ int main() {
 
 	// Pass (start mode) task list to Giotto director to run simulation
 	// .......
-	// set mode list & set start mode ???
-	// Initialize tasks and stuff, get a schedule for the given mode,
-	// and start the director running...
-	// vector<Mode*> modeVector;
-	// modeVector.push_back(lowRainMode);
-	// modeVector.push_back(medRainMode);
-	// modeVector.push_back(highRainMode);
+	// Initialize director with start mode and kick off simulation
 	GiottoDirector giottoD = new GiottoDirector(lowRainMode);
-	// port and driver lists ???
 	giottoD.Run();
-
-
-
 
 	// // Ptides simulation
 	// PtidesDirector ptidesD = new PtidesDirector();
